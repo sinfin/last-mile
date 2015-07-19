0281f3cea0e843a5d66acf41d0e02cd0ac42011c
Filip Ornstein <filip@sinfin.cz>
2015-07-19 13:15:49 +0200

    * fix requiring dependencies in module

--- lib/last_mile.rb
+++ lib/last_mile.rb
@@ -1,10 +1,9 @@
-require_relative "./last_mile/utils"
-require_relative "./last_mile/version"
-require_relative "./last_mile/deploy"
-require_relative "./last_mile/digital_ocean"
-
-
 module LastMile
+  require_relative "./last_mile/utils"
+  require_relative "./last_mile/version"
+  require_relative "./last_mile/deploy"
+  require_relative "./last_mile/digital_ocean"
+  
   include LastMile::DigitalOcean
   include LastMile::Deploy
 end
